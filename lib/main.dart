d9e7a609d156ab3781065634fca1e48d1ba3f28d
Vinicius <Viniciusmaboni@hotmail.com>
2020-04-28 20:24:34 -0300

    criado loading button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aog/widgets/input.widget.dart';
+import 'package:aog/widgets/loading-button.widget.dart';
 import 'package:aog/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
@@ -31,27 +32,44 @@ class Home extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          Input(label: "Gasolina", controller: _gasolinaController),
-          Input(label: "Alcool", controller: _gasolinaController),
           Container(
-            height: 60,
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child: FlatButton(
-              child: Text(
-                "CALCULAR",
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 25,
-                  fontFamily: "Big Shoulders Display",
-                ),
+              margin: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(50),
               ),
-              onPressed: () {},
-            ),
-          )
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    "COMPENSA USAR ALCOOL",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 25,
+                      fontFamily: "Big Shoulders Display",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  LoadingButton(
+                    text: "Calcular Novamente",
+                    func: () {},
+                    busy: false,
+                    invert: true,
+                  ),
+                ],
+              )),
+          Input(label: "Gasolina", controller: _gasolinaController),
+          Input(label: "Alcool", controller: _alcoolController),
+          LoadingButton(
+            text: "Calcular",
+            func: () {},
+            busy: false,
+            invert: false,
+          ),
         ],
       ),
     );
